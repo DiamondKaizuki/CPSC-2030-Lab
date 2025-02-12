c3fe4e76069a8a38e46900c5ce2c8f0af359bf3c
Caedmon Dong <caedmonwolf28@gmail.com>
2025-02-12 17:25:50 -0500

    Final Commit

--- project.py
+++ project.py
@@ -2,13 +2,21 @@ import time
 
 print("Hello there, before you continue here's a quick question")
 print("Are you a human being?")
+
 while True:
-	userinput = input("\nType Y for yes\nType N for no\nPlease Respond: ")
-	I1 = userinput.lower()
-if I1 == "y":
-	print("\nWelcome User")
+    userinput = input("\nType Y for yes\nType N for no\nPlease Respond: ")
+    I1 = userinput.lower()
+    
+    if I1 == "y":
+        time.sleep(1)
+        print("\nWelcome User")
         break
-elif I1 == "n":
+
+    elif I1 == "n":
+        time.sleep(1)
         print("\nPlease Bring A Human Being To The Machine")
-else:
+        time.sleep(1)
+
+    else:
+        time.sleep(1)
         print("\nPlease Enter Either Y or N")
