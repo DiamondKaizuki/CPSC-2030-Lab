91f83b25f75fface4c9fc2311fc0ed18b99064c7
Caedmon Dong <caedmonwolf28@gmail.com>
2025-02-12 17:13:37 -0500

    Project v 0.

---
+++ project.py
@@ -0,0 +1,5 @@
+print("Hello there, before you continue here's a quick question")
+print("Are you a human being?")
+while True:
+    userinput = input("\nType Y for yes\nType N for no\nPlease Respond: ")
+    I1 = userinput.lower()
